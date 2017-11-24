3a8c41454afc10d9cb96dc449ebe6d49fded29b2
Unknown <jcsouza.first@gmail.com>
2017-11-24 13:27:56 -0200

    Adding the beta app for the project

--- src/main/java/br/com/pm_2017/si_grade/io/Si_Grade_App.java
+++ src/main/java/br/com/pm_2017/si_grade/io/Si_Grade_App.java
@@ -1,60 +1,47 @@
 package br.com.pm_2017.si_grade.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
+import br.com.pm_2017.si_grade.controller.StudentController;
 import br.com.pm_2017.si_grade.model.Student;
-import br.com.pm_2017.si_grade.parser.StudentParser;
 
-public class TesteTT {
+public class Si_Grade_App {
 
-	public static void main(String[] args) {
-		final Pattern path = Pattern.compile("^(\\s){4}<path(\\s)id");
-		Path wiki_path = Paths.get("svg.svg");
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		StudentController studentController = new StudentController();
 		
-		Charset charset = Charset.forName("UTF-8");
-		List<String> lines;
-		try {
-			PDFIO pdfIo = new PDFIO("historico.pdf");
-			lines = Files.readAllLines(wiki_path, charset);
-			int j = 1;
-			
-			for(String line: pdfIo.getLinesList()) {
-				System.out.println(line);
-				
-				//Matcher matcher = path.matcher(line);
-				//if(matcher.find()) {
-					//System.out.println("Linha"+j + " :" + line);
-					//j++;
-				//}	
-			}
-			StudentParser sp = new StudentParser();
-			Student student = sp.generateStudent( pdfIo.getLinesList() );
-			System.out.println( student.toString());
-			List<String> teste = new ArrayList<String>();
-			if(teste.isEmpty())
-				System.out.println("Lista null vale como empty");
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+		List<String> document = studentController.generateDocumentLinesList("historico2.pdf");//modificar para generateDocumentLinesList
+		
+		Student student = studentController.getStudentFromPdf(document);
+		Map<String, Boolean> rulesCache = studentController.generateRuleMap(student);
 
-	}
-	public static List<String> svgLinesPreTest() throws IOException {
-		FileReader fr = new FileReader("svg.svg");
-		BufferedReader br = new BufferedReader(fr);
-		List<String> list = new ArrayList<String>();
-		while( br.ready() )
-			list.add(br.readLine());
-		br.close();
-		return list;
+		List<String> lines = studentController.generateSvgStringList("svg.svg");//generateSvgLinesList
+		
+		
+		String linesAsString = studentController.generateColoredSvg(student, lines);
+		
+		List<String> html = studentController.generateHtmlLinesList("si_grade.html");
+		
+		List<String> finalHtml = studentController.generateHtmlPageForStudent(html, student, rulesCache, linesAsString)	;	
+		String finalHtmlAsString = String.join("\n", finalHtml);
+		//System.out.println(finalHtmlAsString);
+		File file = File.createTempFile("grade-" + student.getName(), ".html" );
+		// Delete temp file when program exits.
+	    file.deleteOnExit();
+
+	    // Write to temp file
+	    BufferedWriter out = new BufferedWriter(new FileWriter(file));
+	    out.write(finalHtmlAsString);
+	    out.close();		
+		if(Desktop.isDesktopSupported()) {
+			Desktop.getDesktop().browse(file.toURI());
+		}
 	}
 }
